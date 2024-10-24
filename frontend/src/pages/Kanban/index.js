09bd18e49c58f6a296262dc770bcc0417908230b
Abraao Martins <abraao.neto@souunit.com.br>
2024-10-24 16:16:43 -0300

    Remoção do kanban antigo

--- frontend/src/pages/Kanban/index.js
+++ frontend/src/pages/Kanban/index.js
@@ -6,18 +6,6 @@ import Board from 'react-trello';
 import { toast } from "react-toastify";
 import { i18n } from "../../translate/i18n";
 import { useHistory } from 'react-router-dom';
-<<<<<<< HEAD
-import './kanban.css'
-import mulher2 from '../../assets/mulher2.png';
-import mulher3 from '../../assets/kanban/mulher3.png';
-import homem1 from '../../assets/homem1.png';
-import homem2 from '../../assets/kanban/homem2.png';
-import homem3 from '../../assets/kanban/homem3.png';
-import retanguloazul2 from '../../assets/kanban/retanguloazul2.png';
-import retanguloverde from '../../assets/kanban/retanguloverde.png';
-
-=======
->>>>>>> main
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -33,7 +21,7 @@ const useStyles = makeStyles(theme => ({
     fontWeight: "bold",
     borderRadius: "5px",
   },
-  
+
 }));
 
 const Kanban = () => {
@@ -188,220 +176,14 @@ const Kanban = () => {
 
   return (
     <div className={classes.root}>
-<<<<<<< HEAD
       <Board
         data={file}
         onCardMoveAcrossLanes={handleCardMove}
         style={{ backgroundColor: 'rgba(252, 252, 252, 0.03)' }}
       />
-=======
-      <Board 
-		data={file} 
-		onCardMoveAcrossLanes={handleCardMove}
-		style={{backgroundColor: 'rgba(252, 252, 252, 0.03)'}}
-    />
->>>>>>> main
     </div>
   );
 };
 
 
-<<<<<<< HEAD
 export default Kanban;
-
-
-/*
-function kanbanColumns() {
-  return (
-    <body>
-      <div className="kanban">
-        <div className="column">
-          <span className="textcolumntasks">Tarefas</span>
-          <div className="item" draggable="true">
-            <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Criar o Figma</span>          
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-            <div class="anexocome">
-              <img src="public/external/raphaelclip3251-u9pq.svg" alt="" className="anexo" />
-              <img src="public/external/materialsymbolslightchatoutline3251-4pqp.svg" alt="" className="comentario" />
-            </div>
-          </div>
-          <div className="item" draggable="true">
-           <div className="retangulosdoitem">
-               <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-               <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Criar backlog</span>
-            <div className="data"><span>15 de abril</span></div>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Organizar entrega parcial</span>
-            <div className="data"><span>08 de março</span></div>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Rodar Backend</span>
-            <div className="data"><span>01 de fevereiro</span></div>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-        </div>
-        <div className="column">
-          <span className="textcolumntasks">Em aberto</span>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Criar o Figma</span>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Criar backlog</span>
-            <div className="data"><span>15 de abril</span></div>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>      
-          </div>
-        </div>
-        <div className="column">
-          <span className="textcolumntasks">Em andamento</span>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem1">Criar o Figma</span>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Criar backlog</span>
-            <div className="data"><span>15 de abril</span></div>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Organizar entrega parcial</span>
-            <div className="data"><span>08 de março</span></div>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-        </div>
-        <div className="column">
-          <span className="textcolumntasks">Concluído</span>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Criar o Figma</span>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Criar backlog</span>
-            <span className="data">15 de abril</span>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>     
-          </div>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Organizar entrega parcial</span>
-            <span className="data">08 de março</span>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>  
-          </div>
-          <div className="item" draggable="true">
-          <div className="retangulosdoitem">
-              <img src={retanguloazul2} alt="retanguloazul" className="retanguloazul2"></img>
-              <img src={retanguloverde} className="reatanguloverde"></img>
-            </div>
-            <span className="textoitem">Rodar Backend</span>
-            <div className="data"><span>01 de fevereiro|</span></div>
-            <div className="pessoas">
-              <img className="homem1" src={homem1} alt="homem1"></img>
-              <img className="mulher2" src={mulher2} alt="mulher2"></img>
-              <img className="mulher3" src={mulher3} alt="mulher3"></img>
-            </div>
-          </div>
-        </div>
-      </div>
-    </body>
-  );
-}
-export default kanbanColumns;
-*/ 
-=======
-export default Kanban;
->>>>>>> main
